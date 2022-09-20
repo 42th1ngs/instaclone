25db6b28e7318a4dc07aa5ed41b81ce0f5e9dda0
Megan Carisa <meganchoy@gmail.com>
2022-09-19 23:23:39 -0700

    Folder Setup

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
 );
+
+
+// client side rendered app: react (cra)
+  // -> database which is Firebase
+  // -> react0loading-skeleton
+  // -> tailwind
+
+// architecture
+  // -> src
+    // -> components
+    // -> constants
+    // -> context
+    // -> helpers
+    // -> hooks
+    // -> lib (firebase is going to live here)
+    // -> pages
+    // -> services (firebase functions in here)
+    // -> styles (tailwind's folder (app/tailwind))
