c6ef70d66a8c1851c03b6a581a4adacd3bab7f57
Megan Carisa <meganchoy@gmail.com>
2022-09-22 00:05:34 -0700

    Login page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,27 @@ module.exports = {
     content: [
         'src/pages/**/*.{html,js}'
     ],
+    theme: {
+        fill: (theme) => ({
+            red: theme('colors.red.primary')
+        }),
+        colors: {
+            white: '#ffffff',
+            blue: {
+                medium: '#005c98'
+            },
+            black: {
+                light: '#262626',
+                faded: '#00000059'
+            },
+            gray: {
+                base: '#616161',
+                background: '#fafafa',
+                primary: '#dbdbdb'
+            },
+            red: {
+                primary: '#ed4956'
+            }
+        }
+    }
 };
