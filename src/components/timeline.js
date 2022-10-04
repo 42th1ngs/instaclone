e0f7f6ac9b8faeb7f895f40d02c49a4b9ea73a51
Megan Carisa <meganchoy@gmail.com>
2022-10-03 22:58:05 -0700

    use-photos hook complete

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,27 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function TImeline() {
+
+    // we need to get the logged in user's photos (hook)
+    const { photos } = usePhotos();
+
+    console.log('photos', photos);
+    // on loading hte photos, use react skeleton
+    // if we have photos, render them (create a post component)
+    // if the user has no photos, tell them to create some photos
+
     return (
         <div className="container col-span-2">
-            <p>I am the timeline</p>
+            {!photos ? (
+                <>
+                    <Skeleton count={4} width={640} height={500} className="mb-5" />
+                </>
+            ) : photos?.length > 0 ? (
+                photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+            ) : (
+                <p className="text-center text-2xl">Follow people to see photos!</p>
+            )}
         </div>
     );
 }
