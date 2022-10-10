2a3a348046468d7cc7d2f2661460adcac5b51954
Megan Carisa <meganchoy@gmail.com>
2022-10-09 21:44:30 -0700

    profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,34 @@
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Profile() {
     const { username } = useParams();
-    console.log('username', username);
-    return <p>hello</p>;
+    const [user, setUser] = useState(null);
+    const [userExists, setUserExists] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function checkUserExists() {
+            const user = await getUserByUsername(username);
+            if (user.length > 0) {
+                setUser(user);
+                setUserExists(true);
+            } else {
+                setUserExists(false);
+                navigate(ROUTES.NOT_FOUND);
+            }
+        }
+
+        checkUserExists();
+    }, [username, history]);
+
+    return userExists ? (
+        <div className="bg-gray-background">
+            <div className="mx-auto max-w-screen-lg">
+                {username}
+            </div>
+        </div>
+    ) : null;
 }
